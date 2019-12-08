76580ea5f12b50eb36d6a8984840914b940a2192
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2019-12-09 00:10:55 +0900

    add options

--- copy_result.zsh
+++ copy_result.zsh
@@ -6,13 +6,17 @@ if (( ! $+commands[pbcopy] )); then
    exit 1
 fi
 
-if (( $# < 2 )); then
+zparseopts -D -M -A opthash -- \
+   -help h=-help \
+   -reset r=-reset
+
+if [[ $+opthash[(i)--help] ]] || (( $# < 2 )); then
    print -u 2 -- "usage: $0 megido_name evolution_num"
    exit 1
 fi
 
 local result=result/$1.txt
-if [[ ! -f $result ]]; then
+if [[ $+opthash[(i)--reset] ]] || [[ ! -f $result ]]; then
    ./count_item.rb $1
 fi
 cat $result | head -n $2 | pbcopy
