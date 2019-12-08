a11af4bf589187d4fb2ab429c77665083441c41f
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2019-12-09 00:15:40 +0900

    bugfix

--- copy_result.zsh
+++ copy_result.zsh
@@ -1,22 +1,23 @@
 #!/usr/bin/env zsh
 set -eu
 
-if (( ! $+commands[pbcopy] )); then
+if (( ! $+commands[pbcopy] )) && (( ! $+aliases[pbcopy] )); then
    print -u 2 -- "Command 'pbcopy' not found. Please consider using alias"
    exit 1
 fi
 
+local -A opthash
 zparseopts -D -M -A opthash -- \
    -help h=-help \
    -reset r=-reset
 
-if [[ $+opthash[(i)--help] ]] || (( $# < 2 )); then
+if (( $+opthash[--help] )) || (( $# < 2 )); then
    print -u 2 -- "usage: $0 megido_name evolution_num"
    exit 1
 fi
 
 local result=result/$1.txt
-if [[ $+opthash[(i)--reset] ]] || [[ ! -f $result ]]; then
+if (( $+opthash[--reset] )) || [[ ! -f $result ]]; then
    ./count_item.rb $1
 fi
 cat $result | head -n $2 | pbcopy
